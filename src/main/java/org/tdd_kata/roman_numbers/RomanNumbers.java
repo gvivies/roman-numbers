848e4a9be803aab3c21ede13bf11a8515f0086d3
Gilles Vivies <gilles.vivies@gmail.com>
2018-04-01 00:18:46 +0200

    Second try

--- src/main/java/org/tdd_kata/roman_numbers/RomanNumbers.java
+++ src/main/java/org/tdd_kata/roman_numbers/RomanNumbers.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 public class RomanNumbers {
 
 	private static HashMap<Integer, String> CONVERSION_TABLE = new HashMap<Integer, String>();
-
 	private String result = "";
-	private int restToConvert = 0;
+	private int rest = 0;
 
 	public RomanNumbers() {
 		CONVERSION_TABLE.put(1, "I");
@@ -19,79 +18,92 @@ public class RomanNumbers {
 		CONVERSION_TABLE.put(1000, "M");
 	}
 
-	public String convert(int number) {
+	public String convert(int number) throws IllegalArgumentException {
 
-		if (numberExistsInConversionMap(number)) {
+		if (numberIsInvalid(number)) {
+			throw new IllegalArgumentException("Number is invalid");
+		}
+		if (numberIsInConversionMap(number)) {
 			return CONVERSION_TABLE.get(number);
 		}
 
-		initWorkingVariables(number);
-
-		applyThousandConversion();
-		applyHundredConversion();
-		applyDecadeConversion();
-		applyUnitsConversion();
+		init(number) //
+				.then() //
+				.convertThousands() //
+				.then() //
+				.convertHundreds() //
+				.then() //
+				.convertDecades() //
+				.then() //
+				.convertUnits();
 
 		return result;
 	}
 
-	private void applyThousandConversion() {
-		applyMultipleConversion(1000);
+	private boolean numberIsInvalid(int number) {
+		return number < 1 || number > 4999;
 	}
 
-	private void applyHundredConversion() {
-		applyMultipleConversion(100);
-	}
-
-	private void applyDecadeConversion() {
-		applyMultipleConversion(10);
-	}
-
-	private void applyMultipleConversion(int base) {
-		if (restToConvert > base) {
-			int rounded = (int) Math.floor(restToConvert / base);
-			result = result.concat(convertBaseN(rounded, base));
-			restToConvert -= rounded * base;
-		}
-	}
-
-	private void applyUnitsConversion() {
-		if (restToConvert % 10 != 0) {
-			result = result.concat(convertBaseN(restToConvert, 1));
-		}
-	}
-
-	private String convertBaseN(int number, int multiple) {
-		String tenthSymbol = CONVERSION_TABLE.get(1 * multiple);
-		String initialSymbol = tenthSymbol;
-		String pivotSymbol = (multiple == 1000) ? "MMM" : CONVERSION_TABLE.get(5 * multiple);
-		String upperSymbol = CONVERSION_TABLE.get(10 * multiple);
-		String convertedBaseN = "";
-		if (number == 9) {
-			convertedBaseN = tenthSymbol + upperSymbol;
-		} else if (number >= 5) {
-			convertedBaseN = convertUnits(number - 5, pivotSymbol, tenthSymbol);
-		} else if (number == 4) {
-			convertedBaseN = initialSymbol + pivotSymbol;
-		} else {
-			convertedBaseN = convertUnits(number, "", tenthSymbol);
-		}
-		return convertedBaseN;
-	}
-
-	private String convertUnits(int number, String fromSymbol, String unit) {
-		for (int i = 0; i < number; i++) {
-			fromSymbol += unit;
-		}
-		return fromSymbol;
-	}
-
-	private void initWorkingVariables(int number) {
-		result = "";
-		restToConvert = number;
-	}
-
-	private boolean numberExistsInConversionMap(int number) {
+	private boolean numberIsInConversionMap(int number) {
 		return CONVERSION_TABLE.containsKey(number);
+	}
+
+	private RomanNumbers init(int number) {
+		result = "";
+		rest = number;
+		return this;
+	}
+
+	private RomanNumbers convertBaseN(int unit) {
+		if (rest >= unit) {
+			int base = (int) Math.floor(rest / unit);
+			if (base == 9) {
+				result += CONVERSION_TABLE.get(unit) + CONVERSION_TABLE.get(10 * unit);
+			} else if (base == 4) {
+				result += CONVERSION_TABLE.get(unit) + CONVERSION_TABLE.get(5 * unit);
+			} else if (base == 5) {
+				result += CONVERSION_TABLE.get(5 * unit);
+			} else if (base > 5) {
+				result += CONVERSION_TABLE.get(5 * unit);
+				convertLessThan10(unit, base - 5);
+			} else {
+				convertLessThan10(unit, base);
+			}
+			rest -= base * unit;
+		}
+		return this;
+	}
+
+	private void convertLessThan10(int unit, int base) {
+		for (int i = 0; i < base; i++) {
+			result += CONVERSION_TABLE.get(unit);
+		}
+	}
+
+	private RomanNumbers convertThousands() {
+		if (rest >= 1000) {
+			int thousands = (int) Math.floor(rest / 1000);
+			for (int i = 0; i < thousands; i++) {
+				result += CONVERSION_TABLE.get(1000);
+			}
+			rest -= thousands * 1000;
+		}
+		return this;
+	}
+
+	private RomanNumbers convertHundreds() {
+		return convertBaseN(100);
+	}
+
+	private RomanNumbers convertDecades() {
+		return convertBaseN(10);
+	}
+
+	private RomanNumbers convertUnits() {
+		return convertBaseN(1);
+	}
+
+	private RomanNumbers then() {
+		return this;
 	}
 }
--- src/test/java/org/tdd_kata/roman_numbers/RomanNumbersTest.java
+++ src/test/java/org/tdd_kata/roman_numbers/RomanNumbersTest.java
@@ -1,13 +1,18 @@
 package org.tdd_kata.roman_numbers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import junit.framework.TestCase;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class RomanNumbersTest {
 
 	private RomanNumbers romanNumbers = new RomanNumbers();
-	
+
 	@Test
 	public void conversionTableShouldBeComplete() throws Exception {
 		assertThat(romanNumbers.convert(1)).isEqualTo("I");
@@ -18,297 +23,53 @@ public class RomanNumbersTest {
 		assertThat(romanNumbers.convert(500)).isEqualTo("D");
 		assertThat(romanNumbers.convert(1000)).isEqualTo("M");
 	}
-	
-	@Test
-	public void valuesLessThan4ShouldBeConvertedWith_n_I() throws Exception {
-		assertThat(romanNumbers.convert(2)).isEqualTo("II");
-		assertThat(romanNumbers.convert(3)).isEqualTo("III");
-	}
-	
-	@Test
-	public void value4ShouldBeConvertedWith_IV() throws Exception {
-		assertThat(romanNumbers.convert(4)).isEqualTo("IV");
-	}
-	
-	@Test
-	public void valuesBetween6And8ShouldBeConvertedWith_V_and_n_I() throws Exception {
-		assertThat(romanNumbers.convert(6)).isEqualTo("VI");
-		assertThat(romanNumbers.convert(7)).isEqualTo("VII");
-		assertThat(romanNumbers.convert(8)).isEqualTo("VIII");
-	}
 
 	@Test
-	public void value9ShouldBeConvertedWith_IX() throws Exception {
-		assertThat(romanNumbers.convert(9)).isEqualTo("IX");
-	}
-	
-	@Test
-	public void valuesBetween11And19ShouldBeConvertedWith_X_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(11)).isEqualTo("XI");
-		assertThat(romanNumbers.convert(12)).isEqualTo("XII");
-		assertThat(romanNumbers.convert(14)).isEqualTo("XIV");
-		assertThat(romanNumbers.convert(15)).isEqualTo("XV");
-		assertThat(romanNumbers.convert(17)).isEqualTo("XVII");
-		assertThat(romanNumbers.convert(19)).isEqualTo("XIX");
-	}
-	
-	@Test
-	public void value20ShouldBeConvertedWith_XX() throws Exception {
-		assertThat(romanNumbers.convert(20)).isEqualTo("XX");
-	}
-
-	@Test
-	public void valuesBetWeen21And29ShouldBeConvertedWith_XX_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(21)).isEqualTo("XXI");
-		assertThat(romanNumbers.convert(23)).isEqualTo("XXIII");
-		assertThat(romanNumbers.convert(24)).isEqualTo("XXIV");
-		assertThat(romanNumbers.convert(25)).isEqualTo("XXV");
-		assertThat(romanNumbers.convert(27)).isEqualTo("XXVII");
-		assertThat(romanNumbers.convert(29)).isEqualTo("XXIX");
-	}
-	
-	@Test
-	public void valuesBetWeen31And39ShouldBeConvertedWith_XX_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(31)).isEqualTo("XXXI");
-		assertThat(romanNumbers.convert(33)).isEqualTo("XXXIII");
-		assertThat(romanNumbers.convert(34)).isEqualTo("XXXIV");
-		assertThat(romanNumbers.convert(35)).isEqualTo("XXXV");
-		assertThat(romanNumbers.convert(37)).isEqualTo("XXXVII");
-		assertThat(romanNumbers.convert(39)).isEqualTo("XXXIX");
-	}
-	
-	@Test
-	public void value40ShouldBeConvertedWith_XL() throws Exception {
-		assertThat(romanNumbers.convert(40)).isEqualTo("XL");
-	}
-	
-	@Test
-	public void valuesBetWeen41And49ShouldBeConvertedWith_XL_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(41)).isEqualTo("XLI");
-		assertThat(romanNumbers.convert(43)).isEqualTo("XLIII");
-		assertThat(romanNumbers.convert(44)).isEqualTo("XLIV");
-		assertThat(romanNumbers.convert(45)).isEqualTo("XLV");
-		assertThat(romanNumbers.convert(47)).isEqualTo("XLVII");
-		assertThat(romanNumbers.convert(49)).isEqualTo("XLIX");
-	}
-	
-	@Test
-	public void value51ShouldBeConvertedWith_LI() throws Exception {
-		assertThat(romanNumbers.convert(51)).isEqualTo("LI");
-	}
-	
-	@Test
-	public void valuesBetWeen51And59ShouldBeConvertedWith_L_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(51)).isEqualTo("LI");
-		assertThat(romanNumbers.convert(53)).isEqualTo("LIII");
-		assertThat(romanNumbers.convert(54)).isEqualTo("LIV");
-		assertThat(romanNumbers.convert(55)).isEqualTo("LV");
-		assertThat(romanNumbers.convert(56)).isEqualTo("LVI");
-		assertThat(romanNumbers.convert(59)).isEqualTo("LIX");
-	}
-	
-	@Test
-	public void value60ShouldBeConvertedWith_LX() throws Exception {
-		assertThat(romanNumbers.convert(60)).isEqualTo("LX");
-	}
-	
-	@Test
-	public void valuesBetWeen61And69ShouldBeConvertedWith_LX_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(61)).isEqualTo("LXI");
-		assertThat(romanNumbers.convert(63)).isEqualTo("LXIII");
-		assertThat(romanNumbers.convert(64)).isEqualTo("LXIV");
-		assertThat(romanNumbers.convert(65)).isEqualTo("LXV");
-		assertThat(romanNumbers.convert(66)).isEqualTo("LXVI");
-		assertThat(romanNumbers.convert(69)).isEqualTo("LXIX");
-	}
-	
-	@Test
-	public void value70ShouldBeConvertedWith_LXX() throws Exception {
-		assertThat(romanNumbers.convert(70)).isEqualTo("LXX");
-	}
-	
-	@Test
-	public void valuesBetWeen71And79ShouldBeConvertedWith_LXX_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(71)).isEqualTo("LXXI");
-		assertThat(romanNumbers.convert(73)).isEqualTo("LXXIII");
-		assertThat(romanNumbers.convert(74)).isEqualTo("LXXIV");
-		assertThat(romanNumbers.convert(75)).isEqualTo("LXXV");
-		assertThat(romanNumbers.convert(76)).isEqualTo("LXXVI");
-		assertThat(romanNumbers.convert(79)).isEqualTo("LXXIX");
-	}
-
-	@Test
-	public void value80ShouldBeConvertedWith_LXXX() throws Exception {
-		assertThat(romanNumbers.convert(80)).isEqualTo("LXXX");
-	}
-	
-	@Test
-	public void valuesBetWeen81And89ShouldBeConvertedWith_LXXX_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(81)).isEqualTo("LXXXI");
-		assertThat(romanNumbers.convert(83)).isEqualTo("LXXXIII");
-		assertThat(romanNumbers.convert(84)).isEqualTo("LXXXIV");
-		assertThat(romanNumbers.convert(85)).isEqualTo("LXXXV");
-		assertThat(romanNumbers.convert(87)).isEqualTo("LXXXVII");
-		assertThat(romanNumbers.convert(89)).isEqualTo("LXXXIX");
-	}
-	
-	@Test
-	public void value90ShouldBeConvertedWith_XC() throws Exception {
-		assertThat(romanNumbers.convert(90)).isEqualTo("XC");
-	}
-	
-	@Test
-	public void valuesBetWeen91And99ShouldBeConvertedWith_XC_and_units_converted_symbols() throws Exception {
-		assertThat(romanNumbers.convert(91)).isEqualTo("XCI");
-		assertThat(romanNumbers.convert(93)).isEqualTo("XCIII");
-		assertThat(romanNumbers.convert(94)).isEqualTo("XCIV");
-		assertThat(romanNumbers.convert(95)).isEqualTo("XCV");
-		assertThat(romanNumbers.convert(97)).isEqualTo("XCVII");
-		assertThat(romanNumbers.convert(99)).isEqualTo("XCIX");
-	}
-
-	@Test
-	public void value100ShouldBeConvertedWith_C() throws Exception {
-		assertThat(romanNumbers.convert(100)).isEqualTo("C");
-	}
-	
-	@Test
-	public void valuesFrom101To201ShouldBeConvertedWith_C_and_same_as_previously() throws Exception {
-		assertThat(romanNumbers.convert(101)).isEqualTo("CI");
-		assertThat(romanNumbers.convert(109)).isEqualTo("CIX");
-		assertThat(romanNumbers.convert(114)).isEqualTo("CXIV");
-		assertThat(romanNumbers.convert(123)).isEqualTo("CXXIII");
-		assertThat(romanNumbers.convert(133)).isEqualTo("CXXXIII");
-		assertThat(romanNumbers.convert(140)).isEqualTo("CXL");
-		assertThat(romanNumbers.convert(149)).isEqualTo("CXLIX");
-		assertThat(romanNumbers.convert(155)).isEqualTo("CLV");
-		assertThat(romanNumbers.convert(163)).isEqualTo("CLXIII");
-		assertThat(romanNumbers.convert(174)).isEqualTo("CLXXIV");
-		assertThat(romanNumbers.convert(188)).isEqualTo("CLXXXVIII");
-		
-		assertThat(romanNumbers.convert(191)).isEqualTo("CXCI");
-		assertThat(romanNumbers.convert(193)).isEqualTo("CXCIII");
-		assertThat(romanNumbers.convert(194)).isEqualTo("CXCIV");
-		assertThat(romanNumbers.convert(195)).isEqualTo("CXCV");
-		assertThat(romanNumbers.convert(197)).isEqualTo("CXCVII");
-		assertThat(romanNumbers.convert(199)).isEqualTo("CXCIX");
-	}
-	
-	@Test
-	public void value200ShouldBeConvertedWith_CC() throws Exception {
-		assertThat(romanNumbers.convert(200)).isEqualTo("CC");
-		assertThat(romanNumbers.convert(201)).isEqualTo("CCI");
-		assertThat(romanNumbers.convert(209)).isEqualTo("CCIX");
-		assertThat(romanNumbers.convert(214)).isEqualTo("CCXIV");
-		assertThat(romanNumbers.convert(223)).isEqualTo("CCXXIII");
-		assertThat(romanNumbers.convert(233)).isEqualTo("CCXXXIII");
-		assertThat(romanNumbers.convert(240)).isEqualTo("CCXL");
-		assertThat(romanNumbers.convert(249)).isEqualTo("CCXLIX");
-		assertThat(romanNumbers.convert(255)).isEqualTo("CCLV");
-		assertThat(romanNumbers.convert(263)).isEqualTo("CCLXIII");
-		assertThat(romanNumbers.convert(274)).isEqualTo("CCLXXIV");
-		assertThat(romanNumbers.convert(288)).isEqualTo("CCLXXXVIII");
-		assertThat(romanNumbers.convert(290)).isEqualTo("CCXC");
-		assertThat(romanNumbers.convert(294)).isEqualTo("CCXCIV");
-		assertThat(romanNumbers.convert(295)).isEqualTo("CCXCV");
-		assertThat(romanNumbers.convert(297)).isEqualTo("CCXCVII");
-	}
-
-	@Test
-	public void value300SHouldBeConvertedToCCC() throws Exception {
-		assertThat(romanNumbers.convert(300)).isEqualTo("CCC");
-		assertThat(romanNumbers.convert(301)).isEqualTo("CCCI");
-		assertThat(romanNumbers.convert(309)).isEqualTo("CCCIX");
-		assertThat(romanNumbers.convert(314)).isEqualTo("CCCXIV");
-		assertThat(romanNumbers.convert(323)).isEqualTo("CCCXXIII");
-		assertThat(romanNumbers.convert(333)).isEqualTo("CCCXXXIII");
-		assertThat(romanNumbers.convert(340)).isEqualTo("CCCXL");
-		assertThat(romanNumbers.convert(349)).isEqualTo("CCCXLIX");
-		assertThat(romanNumbers.convert(355)).isEqualTo("CCCLV");
-		assertThat(romanNumbers.convert(363)).isEqualTo("CCCLXIII");
-		assertThat(romanNumbers.convert(374)).isEqualTo("CCCLXXIV");
-		assertThat(romanNumbers.convert(388)).isEqualTo("CCCLXXXVIII");
-		assertThat(romanNumbers.convert(390)).isEqualTo("CCCXC");
-		assertThat(romanNumbers.convert(394)).isEqualTo("CCCXCIV");
-		assertThat(romanNumbers.convert(395)).isEqualTo("CCCXCV");
-		assertThat(romanNumbers.convert(397)).isEqualTo("CCCXCVII");
-	}
-	
-	@Test
-	public void value400ShouldBeConvertedToCD() throws Exception {		
-		assertThat(romanNumbers.convert(400)).isEqualTo("CD");
-		assertThat(romanNumbers.convert(401)).isEqualTo("CDI");
-		assertThat(romanNumbers.convert(409)).isEqualTo("CDIX");
-		assertThat(romanNumbers.convert(414)).isEqualTo("CDXIV");
-		assertThat(romanNumbers.convert(423)).isEqualTo("CDXXIII");
-		assertThat(romanNumbers.convert(433)).isEqualTo("CDXXXIII");
-		assertThat(romanNumbers.convert(440)).isEqualTo("CDXL");
-		assertThat(romanNumbers.convert(449)).isEqualTo("CDXLIX");
-		assertThat(romanNumbers.convert(455)).isEqualTo("CDLV");
-		assertThat(romanNumbers.convert(463)).isEqualTo("CDLXIII");
-		assertThat(romanNumbers.convert(474)).isEqualTo("CDLXXIV");
-		assertThat(romanNumbers.convert(488)).isEqualTo("CDLXXXVIII");
-		assertThat(romanNumbers.convert(490)).isEqualTo("CDXC");
-		assertThat(romanNumbers.convert(494)).isEqualTo("CDXCIV");
-		assertThat(romanNumbers.convert(495)).isEqualTo("CDXCV");
-		assertThat(romanNumbers.convert(497)).isEqualTo("CDXCVII");
-	}
-	
-	@Test
-	public void values500ShouldBeConvertedToD() throws Exception {		
-		assertThat(romanNumbers.convert(500)).isEqualTo("D");
-		assertThat(romanNumbers.convert(501)).isEqualTo("DI");
-		assertThat(romanNumbers.convert(509)).isEqualTo("DIX");
-		assertThat(romanNumbers.convert(514)).isEqualTo("DXIV");
-		assertThat(romanNumbers.convert(523)).isEqualTo("DXXIII");
-		assertThat(romanNumbers.convert(533)).isEqualTo("DXXXIII");
-		assertThat(romanNumbers.convert(540)).isEqualTo("DXL");
-		assertThat(romanNumbers.convert(549)).isEqualTo("DXLIX");
-		assertThat(romanNumbers.convert(555)).isEqualTo("DLV");
-		assertThat(romanNumbers.convert(563)).isEqualTo("DLXIII");
-		assertThat(romanNumbers.convert(574)).isEqualTo("DLXXIV");
-		assertThat(romanNumbers.convert(588)).isEqualTo("DLXXXVIII");
-		assertThat(romanNumbers.convert(590)).isEqualTo("DXC");
-		assertThat(romanNumbers.convert(594)).isEqualTo("DXCIV");
-		assertThat(romanNumbers.convert(595)).isEqualTo("DXCV");
-		assertThat(romanNumbers.convert(597)).isEqualTo("DXCVII");
-	}
-	
-	@Test
-	public void values800ShouldBeConvertedToDCCC() throws Exception {		
-		assertThat(romanNumbers.convert(800)).isEqualTo("DCCC");
-		assertThat(romanNumbers.convert(801)).isEqualTo("DCCCI");
-		assertThat(romanNumbers.convert(809)).isEqualTo("DCCCIX");
-		assertThat(romanNumbers.convert(814)).isEqualTo("DCCCXIV");
-		assertThat(romanNumbers.convert(823)).isEqualTo("DCCCXXIII");
-		assertThat(romanNumbers.convert(833)).isEqualTo("DCCCXXXIII");
-		assertThat(romanNumbers.convert(840)).isEqualTo("DCCCXL");
-		assertThat(romanNumbers.convert(849)).isEqualTo("DCCCXLIX");
-		assertThat(romanNumbers.convert(855)).isEqualTo("DCCCLV");
-		assertThat(romanNumbers.convert(863)).isEqualTo("DCCCLXIII");
-		assertThat(romanNumbers.convert(874)).isEqualTo("DCCCLXXIV");
-		assertThat(romanNumbers.convert(888)).isEqualTo("DCCCLXXXVIII");
-		assertThat(romanNumbers.convert(890)).isEqualTo("DCCCXC");
-		assertThat(romanNumbers.convert(894)).isEqualTo("DCCCXCIV");
-		assertThat(romanNumbers.convert(895)).isEqualTo("DCCCXCV");
-		assertThat(romanNumbers.convert(897)).isEqualTo("DCCCXCVII");
-	}
-	
-	@Test
-	public void values900ShouldBeConvertedToCM() throws Exception {
-		assertThat(romanNumbers.convert(900)).isEqualTo("CM");
-		assertThat(romanNumbers.convert(990)).isEqualTo("CMXC");
-		assertThat(romanNumbers.convert(999)).isEqualTo("CMXCIX");
-	}
-	
-	@Test
-	public void value1000ShouldBeConvertedToM() throws Exception {
+	public void thousandsShouldBeConvertedWith_M_Symbols() throws Exception {
 		assertThat(romanNumbers.convert(1000)).isEqualTo("M");
-		assertThat(romanNumbers.convert(1678)).isEqualTo("MDCLXXVIII");
-		assertThat(romanNumbers.convert(3769)).isEqualTo("MMMDCCLXIX");
-		assertThat(romanNumbers.convert(4900)).isEqualTo("MMMMCM");
-		assertThat(romanNumbers.convert(4999)).isEqualTo("MMMMCMXCIX");
-		
+		assertThat(romanNumbers.convert(2000)).isEqualTo("MM");
+		assertThat(romanNumbers.convert(3000)).isEqualTo("MMM");
+		assertThat(romanNumbers.convert(4000)).isEqualTo("MMMM");
 	}
 	
+	@Test
+	public void hundredsShouldBeConvertedWith_I_V_X_L_C_D_And_M_Symbols() throws Exception {
+		assertThat(romanNumbers.convert(1100)).isEqualTo("MC");
+		assertThat(romanNumbers.convert(2200)).isEqualTo("MMCC");
+		assertThat(romanNumbers.convert(3300)).isEqualTo("MMMCCC");
+		assertThat(romanNumbers.convert(3400)).isEqualTo("MMMCD");
+		assertThat(romanNumbers.convert(3500)).isEqualTo("MMMD");
+		assertThat(romanNumbers.convert(3600)).isEqualTo("MMMDC");
+		assertThat(romanNumbers.convert(4800)).isEqualTo("MMMMDCCC");
+		assertThat(romanNumbers.convert(4900)).isEqualTo("MMMMCM");
+	}
+
+	@Test
+	public void decadesShouldBeConvertedWith_I_V_X_L_And_C_Symbols() throws Exception {
+		assertThat(romanNumbers.convert(1110)).isEqualTo("MCX");
+		assertThat(romanNumbers.convert(2220)).isEqualTo("MMCCXX");
+		assertThat(romanNumbers.convert(3330)).isEqualTo("MMMCCCXXX");
+		assertThat(romanNumbers.convert(3440)).isEqualTo("MMMCDXL");
+		assertThat(romanNumbers.convert(3550)).isEqualTo("MMMDL");
+		assertThat(romanNumbers.convert(3560)).isEqualTo("MMMDLX");
+		assertThat(romanNumbers.convert(4990)).isEqualTo("MMMMCMXC");
+	}
+	
+	@Test
+	public void unitsShouldBeConvertedWith_I_V_And_X_Symbols() throws Exception {
+		assertThat(romanNumbers.convert(1)).isEqualTo("I");
+		assertThat(romanNumbers.convert(3)).isEqualTo("III");
+		assertThat(romanNumbers.convert(4)).isEqualTo("IV");
+		assertThat(romanNumbers.convert(5)).isEqualTo("V");
+		assertThat(romanNumbers.convert(6)).isEqualTo("VI");
+		assertThat(romanNumbers.convert(8)).isEqualTo("VIII");
+		assertThat(romanNumbers.convert(9)).isEqualTo("IX");
+
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void valueUpTo5000ShouldBeInvalid() {
+		romanNumbers.convert(5000);
+	}
+
 }
